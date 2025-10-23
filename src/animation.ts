d3d228c9ac9cc0604f8bd9b3743604f9a33d611a
gammii23 <jgamm@ucsd.edu>
2025-10-23 11:14:25 -0700

    Fix spelling: experiance -> experience

--- src/animation.ts
+++ src/animation.ts
@@ -2,7 +2,7 @@
 export function animateFeature(featureName: string): void {
     console.log(`\x1b[1m\x1b[3m🎬 ${featureName} Feature Animation 🎬\x1b[0m`);
     console.log("=".repeat(50));
-    console.log(`\x1b[1m✨ Welcome to the ${featureName} experience! ✨\x1b[0m`);
+    console.log(`\x1b[1m✨ Welcome to the ${featureName} experiance! ✨\x1b[0m`);
     console.log("=".repeat(50));
     console.log("\x1b[3m🎵 Loading amazing content... 🎵\x1b[0m\n");
 }
